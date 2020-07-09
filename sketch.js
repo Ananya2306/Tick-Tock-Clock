a9bfa828d18bc563a94d4c921c8759f362bc93c9
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-07-09 07:41:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+function setup() {
+  createCanvas(450,450);
+  angleMode(DEGREES);
+}
+
+function draw() {
+
+  background(0);
+
+  fill(255);
+  textSize(24);
+  textStyle("bold");
+  text(" ~ Tick - Tock Clock ~  :D", 100,400); 
+  
+
+translate(200,200)
+rotate (-90);
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+  
+  strokeWeight(8);
+  stroke("yellow");
+  noFill();
+  let end = map(sc,0,60,0,360);
+  arc(0,0,300,300,0,end);
+  
+  stroke("Pink");
+  let end2 = map(mn,0,60,0,360);
+  arc(0,0,280,280,0,end2);
+  
+  stroke("Light blue");
+  let end3 = map(hr%12,0,12,-0,360);
+  arc(0,0,260,260,0,end3);
+  
+ /* fill(255);
+  textSize(24);
+  textStyle("bold");
+  text("Time : " + end3 + " : " + end2 + " : " + end , 50 , 400);
+*/
+  push();
+  rotate(end);
+  stroke("blue");
+  line(0,0,100,0);
+  pop();
+  
+  
+  push();
+  rotate(end2);
+  stroke("red");
+  line(0,0,75,0);
+  pop();
+  
+  push();
+  rotate(end3);
+  stroke("green");
+  line(0,0,50,0);
+  pop();
+  
+  stroke("Black");
+  point (0,0);
+
+  
+
+
+  }
+  
+  
